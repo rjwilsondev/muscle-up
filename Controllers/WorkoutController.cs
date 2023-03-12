619906093c64e10ae87e585a19609fa33f3c25a7
rjwilsondev <rjwilson0531@gmail.com>
2023-03-12 14:59:42 -0400

    changed swagger endpoint

--- Controllers/WorkoutController.cs
+++ Controllers/WorkoutController.cs
@@ -8,6 +8,7 @@ public class WorkoutController : Controller
 
     ///<summary>This is a summary</summary>
     [HttpGet("")]
+    [Produces("application/json")]
     [SwaggerResponse(201, "Test", typeof(string))]
     [SwaggerResponse(400, "The data is invalid")]
     public string GetWorkouts()
--- Program.cs
+++ Program.cs
@@ -40,7 +40,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
