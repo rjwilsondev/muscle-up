84b98dd7c2c9f14e87bccb854e54cfb7a13694c0
Ryan Wilson <rjwilson0531@gmail.com>
2023-04-09 20:51:51 -0400

    injected context.

--- src/MuscleUp.Api/Controllers/WorkoutController.cs
+++ src/MuscleUp.Api/Controllers/WorkoutController.cs
@@ -1,15 +1,27 @@
-namespace MuscleUp.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using MuscleUp.Data;
 using Swashbuckle.AspNetCore.Annotations;
+
+namespace MuscleUp.Api.Controllers;
 
 [Route("workout")]
 public class WorkoutController : Controller
 {
+    private readonly WorkoutContext context;
+
+    public WorkoutController(WorkoutContext context) => this.context = context;
+
 
     ///<summary>This is a summary</summary>
     [HttpGet("")]
     [Produces("application/json")]
     [SwaggerResponse(201, "Test", typeof(string))]
     [SwaggerResponse(400, "The data is invalid")]
-    public string GetWorkouts() => "Hello Ryan!";
+    public string GetWorkouts()
+    {
+
+        var workouts = this.context.Workouts.ToList();
+
+        return workouts.Count.ToString();
+    }
 }
